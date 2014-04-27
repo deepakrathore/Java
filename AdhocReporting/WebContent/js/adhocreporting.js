767e4950e34d9592808730e4a7995c05b1d36849
kiranreddyg <kiran.reddyg@gmail.com>
2014-04-27 08:28:48 -0400

    Removing the project

--- AdhocReporting/WebContent/js/adhocreporting.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-	$('#rptCtx1').typeahead({
-	    name: 'countries',
-	    //prefetch: '../data/countries.json',
-	    local: ['timtrueman', 'JakeHarding', 'vskarich'],
-	    limit: 10
-	  });
-});
